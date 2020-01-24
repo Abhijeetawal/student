6f813d0c28924c09c3f6798bb0bf2c4afbfc6607
Abhijeetawal <awalab@sheridancollege.ca>
2020-01-24 13:37:13 -0500

    fourthcommehh

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -18,6 +18,15 @@ public class Student {
     private String name;
     private int id;
     private double abhi;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
     
 
     /**
